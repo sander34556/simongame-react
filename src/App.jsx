4238d19a1c3dbd6790315a55a92d1b814555c177
Sander34556 <der.pakonsak+github@hmail.com>
2025-05-09 11:15:02 +0700

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,36 @@
 import { useState } from "react";
-import Button from "./components/Button"
+import Button from "./components/game/Button";
+import { useGameLogic } from "./hooks/useGameLogic"
 
-const buttonColors = ["red", "blue", "green", "yellow"];
+
 
 const App = () => {
-  const [gamePattern, setGamePatten] = useState([]);
-  const [userClickedPattern, setUserClickedPattern] = useState([]);
-  const [nextPattern, setNextPattern] = useState("");
-  const [isGameOver, setIsGameOver] = useState(false);
+  const {  sequence, handleButtonClick, startGame, level, nextPattern, over, isActive } = useGameLogic();
 
-  const [level, setLevel] = useState(0);
-
-
+  // console.log({ level, sequence, userInput });
+  console.log(sequence);
   return (
-    <div className={`h-screen w-full flex flex-col items-center transition-all duration-300 ${isGameOver ? 'bg-rose-700' : 'bg-zinc-700'}`}>
-      <h1
-        onClick={() => {
-          if (level === 0 || isGameOver) {
-            setIsGameOver(false);
-            nextSequence();
-          }
-        }}
-        className="title text-center my-10 cursor-pointer"
-      >
-        {isGameOver ? "Game Over - Click to Restart" : level === 0 ? "Press to Start" : `Level ${level}`}
-      </h1>
-
-
-      <div className="mt-10 mx-auto">
-        <div>
-          <Button id={'green'} color={'green'} userChosenColour={userChosenColour} nextPattern={nextPattern} checkAnswer={checkAnswer} />
-          <Button id={'red'} color={'red'} userChosenColour={userChosenColour} nextPattern={nextPattern} checkAnswer={checkAnswer} />
-        </div>
-        <div>
-          <Button id={'yellow'} color={'yellow'} userChosenColour={userChosenColour} nextPattern={nextPattern} checkAnswer={checkAnswer} />
-          <Button id={'blue'} color={'blue'} userChosenColour={userChosenColour} nextPattern={nextPattern} checkAnswer={checkAnswer} />
-        </div>
-      </div>
-
-    </div>
-  )
-
-  function userChosenColour(id) {
-    const newPattern = [...userClickedPattern, id];
-    setUserClickedPattern(newPattern);
-    checkAnswer(newPattern, newPattern.length - 1);
-  }
-
-  function nextSequence() {
-    setUserClickedPattern([]);
-    setLevel((lv) => lv + 1);
-
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColour = buttonColors[randomNumber];
-    setGamePatten([...gamePattern, randomChosenColour]);
-
-    setNextPattern(randomChosenColour);
-    setTimeout(() => {
-      setNextPattern(null);
-    }, 100);
-
-    //play sound randomChosenColour
-    playSound(randomChosenColour);
-
-  }
-
-  function playSound(id) {
-    const audio = new Audio(`/sound/${id}.mp3`);
-    audio.play().catch((e) => {
-      console.error('Failed to play sound:', e);
-    });
-  }
-
-  function checkAnswer(pattern, currentLevel) {
-    if (pattern[currentLevel] === gamePattern[currentLevel]) {
-      if (pattern.length === gamePattern.length) {
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
+    <div className={`flex flex-col gap-20 items-center justify-center h-screen ${over ? 'bg-rose-800' : 'bg-neutral-800'}`}>
+      {
+        over ? <h1 className="title" onClick={() => { startGame() }}>GAME OVER</h1>
+          : level === 0
+            ? <h1 className="title" onClick={() => { startGame() }}>Start Game</h1>
+            : <h1 className="title">Level {level}</h1>
       }
-    } else {
-      playSound("wrong");
-      console.log("game-over");
-      setIsGameOver(true);
-      startOver();
-    }
-  }
 
-  function startOver() {
-    setLevel(0);
-    setGamePatten([]);
-    setUserClickedPattern([]);
-  }
-
+      <div className="relative w-[400px] h-[400px] rounded-full ">
+        {[0, 1, 2, 3].map((color) =>
+          <Button
+            key={color}
+            color={color}
+            nextPattern={nextPattern}
+            isActive={isActive}
+            onClick={() => { handleButtonClick(color) }}
+          />
+        )}
+        <div className="absolute w-32 h-32 bg-black rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+      </div>
+    </div >
+  )
 }
 export default App
--- src/components/game/Button.jsx
+++ src/components/game/Button.jsx
@@ -1,21 +1,9 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const colorClasses = {
-    green: 'bg-green-500',
-    red: 'bg-red-500',
-    yellow: 'bg-yellow-500',
-    blue: 'bg-blue-500',
-};
 
-const Button = ({ id, color, userChosenColour, nextPattern, checkAnswer }) => {
+
+const Button = ({ color, onClick, nextPattern, isActive }) => {
     const [btnClick, setBtnClick] = useState(false);
-
-    const hdlClick = () => {
-        userChosenColour(id);
-        playSound(id);
-        animatePress();
-
-    }
 
     const animatePress = () => {
         setBtnClick(true);
@@ -24,19 +12,27 @@ const Button = ({ id, color, userChosenColour, nextPattern, checkAnswer }) => {
         }, 100);
     }
 
+    const hdlClick = () => {
+        onClick()
+        animatePress()
+    }
+
+    const colors = {
+        0: 'bg-green-500 rounded-tl-full top-0 left-0',
+        1: 'bg-red-500  rounded-tr-full top-0 right-0',
+        2: 'bg-yellow-400  rounded-bl-full bottom-0 left-0',
+        3: 'bg-blue-500  rounded-br-full bottom-0 right-0',
+    };
+
+
     return (
         <button
+            disabled={!isActive}
             onClick={() => { hdlClick() }}
-            className={`btn ${colorClasses[color]} ${btnClick ? 'pressed' : ''} ${nextPattern === id ? 'opacity-0' : 'opacity-100'} transition`}
-        />
+            className={`btn ${!isActive ? '' : 'hover:opacity-80 cursor-pointer'} ${colors[color]} ${btnClick ? 'pressed' : ''} ${nextPattern === color ? 'pressed' : ''}`}>
+
+        </button>
 
     )
-
-    function playSound(id) {
-        const audio = new Audio(`/sound/${id}.mp3`);
-        audio.play().catch((e) => {
-            console.error('Failed to play sound:', e);
-        });
-    }
 }
 export default Button
